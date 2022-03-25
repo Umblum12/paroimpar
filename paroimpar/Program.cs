46195104749f7c7c431c487ba2d048751197c31e
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:32:42 -0700

    Agregar archivos de proyecto.

---
+++ paroimpar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace paroimpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introducir número: ");
+            int numero = int.Parse(Console.ReadLine());
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El número " + numero + "es par.");
+            }
+            else
+            {
+                Console.WriteLine("El número " + numero + "es Impar.");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
